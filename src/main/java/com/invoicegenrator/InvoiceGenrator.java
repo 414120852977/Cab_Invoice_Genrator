67282c6dbf68cf4cba82a4ad21119f5609626edf
Ashok Mane <ashokmane81800@gmail.com>
2021-05-12 12:28:25 +0530

    [ashok] added given distance and time should return total fare and minimum fare

--- src/main/java/com/invoicegenrator/InvoiceGenrator.java
+++ src/main/java/com/invoicegenrator/InvoiceGenrator.java
@@ -1,7 +1,17 @@
 package com.invoicegenrator;
 
 public class InvoiceGenrator {
-    public static void main(String[] args) {
-        System.out.println("welcome to cab invoice genrator");
+
+    private static final double MINIMUM_COST_PER_KM = 10.0;
+    private static final int COST_PER_TIME = 1;
+    private static final double MINIMUM_FARE = 5.0;
+
+    public double totalFare(double distance, int time) {
+        double totalFare =  distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
+        if(totalFare < MINIMUM_FARE) {
+            return MINIMUM_FARE;
+        }else {
+            return totalFare;
+        }
     }
 }
--- src/test/java/com/invoicegenrator/InvoiceGenratorTest.java
+++ src/test/java/com/invoicegenrator/InvoiceGenratorTest.java
@@ -1,5 +1,30 @@
 package com.invoicegenrator;
 
-public class InvoiceGenratorTest {
+import org.junit.Assert;
+import org.junit.Test;
 
+public class InvoiceGenratorTest {
+    /**
+     * calculate total fare ......
+     */
+    @Test
+    public void givenDistanceAndTime_ShouldReturn_TotalFare() {
+       InvoiceGenrator invoiceGenrator = new InvoiceGenrator();
+       int time = 5;
+       double distance = 2.0;
+      double fare  =  invoiceGenrator.totalFare(distance,time);
+        Assert.assertEquals(25,fare,0.0);
+    }
+
+    /**
+     * given less distance and time should return mimimum fare.....
+     */
+    @Test
+    public void givenLessDistanceAndTime_ShouldReturnMinFare() {
+        InvoiceGenrator invoiceGenrator = new InvoiceGenrator();
+        int time = 1;
+        double distance = 0.1;
+        double fare  =  invoiceGenrator.totalFare(distance,time);
+        Assert.assertEquals(5,fare,0.0);
+    }
 }
