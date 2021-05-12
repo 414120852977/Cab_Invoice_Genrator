5841867453fdb39d69beaedb3f5f004eb4550aff
Ashok Mane <ashokmane81800@gmail.com>
2021-05-12 12:06:54 +0530

    [ashok] added welcome to cab invoice genrator

---
+++ src/main/java/com/invoicegenrator/InvoiceGenrator.java
@@ -0,0 +1,7 @@
+package com.invoicegenrator;
+
+public class InvoiceGenrator {
+    public static void main(String[] args) {
+        System.out.println("welcome to cab invoice genrator");
+    }
+}
---
+++ src/test/java/com/invoicegenrator/InvoiceGenratorTest.java
@@ -0,0 +1,5 @@
+package com.invoicegenrator;
+
+public class InvoiceGenratorTest {
+
+}
